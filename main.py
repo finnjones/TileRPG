bc7e3abc6e1062dd727ac24d3e970ffe03361dc6
Finn-Jones1 <47406390+Finn-Jones1@users.noreply.github.com>
2021-08-06 10:56:37 +1000

    bad guy and animation

--- main.py
+++ main.py
@@ -13,9 +13,18 @@ clock = pygame.time.Clock()
 FlexyPath = os.path.dirname(os.path.abspath(__file__))
 screenSize = (1620 , 1000)
 window = pygame.display.set_mode(screenSize)
-enemySprite = [pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile011.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile012.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile013.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile014.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile015.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile016.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile017.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile018.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile019.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile0020.png")]
+enemySpriteNsc = [pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile011.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile012.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile013.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile014.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile015.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile016.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile017.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile018.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile019.png"), pygame.image.load(FlexyPath + "/Sprites/Mobs/Cactus/tile020.png")]
+playerSpriteNsc = [pygame.image.load(FlexyPath + "/Sprites/Player/tile014.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile015.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile016.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile017.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile018.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile019.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile020.png"), pygame.image.load(FlexyPath + "/Sprites/Player/tile021.png")]
+enemySprite = []
+playerSprite = []
+for i in enemySpriteNsc:
+    i = pygame.transform.scale(i, (100, 100))
+    enemySprite.append(i)
+for i in playerSpriteNsc:
+    i = pygame.transform.scale(i, (100, 100))
+    playerSprite.append(i)
 
-playerSprite = pygame.image.load(FlexyPath + "/player.png")
+# playerSprite = pygame.image.load(FlexyPath + "/player.png")
 bg = pygame.image.load(FlexyPath + "/map.jpg")
 
 class player(object):
@@ -25,9 +34,13 @@ class player(object):
         self.x = x
         self.y = y
         self.speed = 10
-
+        self.changeSprite = 0
     def draw(self, window):
-        window.blit(playerSprite, (self.x, self.y))
+        if self.changeSprite > 38:
+            self.changeSprite = -1
+        self.changeSprite += 1
+        window.blit(playerSprite[self.changeSprite//5], (self.x, self.y))
+
         
 class enemy(object):
     def __init__(self, x, y, width, height):
@@ -35,8 +48,13 @@ class enemy(object):
         self.height = height
         self.x = x
         self.y = y
+        self.changeSprite = 0
     def draw(self, window):
-        window.blit(enemySprite, (self.x, self.y))
+        if self.changeSprite > 48:
+            self.changeSprite = -1
+        self.changeSprite += 1
+        window.blit(enemySprite[self.changeSprite//5], (self.x, self.y))
+
         
 
 class background(object):
@@ -51,11 +69,13 @@ class background(object):
 def reDraw():
     bgCam.draw(window)
     mainPlayer.draw(window)
+    bad1.draw(window)
     pygame.display.flip()
 
 running = True
 
 bgCam = background(0, 0, 10280, 7019)
+bad1 = enemy(100, 100, 40, 40)
 mainPlayer = player(screenSize[0]/2 - 100/2, screenSize[1]/2 - 91/2, 100, 91)
 while running:
     clock.tick(100)
@@ -71,6 +91,7 @@ while running:
             mainPlayer.y -= mainPlayer.speed
         else:
             bgCam.y -= mainPlayer.speed
+            bad1.y += mainPlayer.speed
 
     if keys[pygame.K_s]:
 
@@ -78,17 +99,23 @@ while running:
             mainPlayer.y += mainPlayer.speed
         else:
             bgCam.y += mainPlayer.speed
+            bad1.y -= mainPlayer.speed
 
     if keys[pygame.K_d]:
         if (bgCam.x - bgCam.height + screenSize[0]) > 0 or mainPlayer.x != screenSize[0]/2 - 100/2:
             mainPlayer.x += mainPlayer.speed
         else:
             bgCam.x += mainPlayer.speed
+            bad1.x -= mainPlayer.speed
 
     if keys[pygame.K_a]:
         if bgCam.x < 0 + 10 or mainPlayer.x != screenSize[0]/2 - 100/2:
             mainPlayer.x -= mainPlayer.speed
         else:
             bgCam.x -= mainPlayer.speed
+            bad1.x += mainPlayer.speed
+    
+    
+
     reDraw()
 pygame.quit()
